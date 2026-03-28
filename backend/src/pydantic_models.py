c563f8bda956912817136cfa6f5fa5a8ac90c62b
William L <frewtypebbles@gmail.com>
2026-03-27 22:02:52 -0700

    pydantic models

--- backend/src/pydantic_models.py
+++ backend/src/pydantic_models.py
@@ -1,1 +1,30 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional 
+from src.project.character import Gender
+from src.project.panel import CameraShot
+
+class CharacterValidator(BaseModel):
+    name:str
+    age:int = Field(gt=0)
+    gender:Gender
+    physical_description:str
+    back_story:str
+    image:str
+
+class PanelValidator(BaseModel):
+    sequence:int
+    camera_shot:Optional[CameraShot]
+    location:Optional[str]
+    time:Optional[str]
+    action:Optional[str]
+    dialogue:Optional[str]
+    caption:Optional[str]
+    image:Optional[str]
+
+class StoryBoardProjectValidator(BaseModel):
+    title:str
+    genre:Optional[str]
+    premise:Optional[str]
+    visual_tone:Optional[str]
+    characters:list[CharacterValidator]
+    panels:list[PanelValidator]
