cebeb80dcef1a32b2696a00a58e68e00ee06e75b
William L <frewtypebbles@gmail.com>
2026-03-27 19:12:14 -0700

    Added SQL_ENGINE to shared.py

--- backend/src/shared.py
+++ backend/src/shared.py
@@ -1,8 +1,11 @@
+from sqlalchemy import create_engine
 from google import genai
 from dotenv import load_dotenv
+
 import os
 
 # This is where our gemini api global variable will go
 load_dotenv()
 
 GEMINI_CLIENT = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+SQL_ENGINE = create_engine("sqlite://", echo=True)
