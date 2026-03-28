c47dea68f72e28e607cca8415100bd6c3cb8d9a4
William L <frewtypebbles@gmail.com>
2026-03-27 19:45:12 -0700

    created character table.

---
+++ backend/src/project/character.py
@@ -0,0 +1,25 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from enum import Enum
+
+
+Base = declarative_base()
+
+class Gender(Enum):
+    MALE = "male"
+    FEMALE = "female"
+
+class Character(Base):
+    __tablename__ = "character"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    story_board_project_id: Mapped[int] = mapped_column(ForeignKey("story_board_project.id"))
+    name: Mapped[str]
+    age: Mapped[int]
+    gender: Mapped[Gender]
+    physical_description: Mapped[str]
+    back_story: Mapped[str]
+    image: Mapped[str] = mapped_column(String(45))
+
+    def __repr__(self):
+        return f"Character(id={self.id!r}, story_board_project_id={self.story_board_project_id!r}, name={self.name!r}, age={self.age!r}, gender={self.gender!r}, physical_description={self.physical_description!r}, back_story={self.back_story!r}, image={self.image!r})"
