d478024257da61b321d842e28887fcd7eae1aa25
William L <frewtypebbles@gmail.com>
2026-03-27 17:58:34 -0700

    added code to shared.py

--- backend/src/shared.py
+++ backend/src/shared.py
@@ -1,3 +1,9 @@
 from google import genai
+from dotenv import load_dotenv
+import os
 
 # This is where our gemini api global variable will go
+load_dotenv()
+
+GEMINI_CLIENT = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+
